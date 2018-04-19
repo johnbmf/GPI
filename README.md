dd060bd8079ddd8ae93833fd643ac78039615189
johnbmf <jonathan.mendoza.14@sansano.usm.cl>
2018-04-19 03:53:21 -0300

    Base Proyecto

---
+++ README.md
@@ -0,0 +1,20 @@
+#JIP - Proyecto GPI
+
+##Antes de clonar:
+-El repositorio funciona dentro de un stack WAMP (Windows, Apache, MySQL, PHP). Para evitar dificultades,
+se recomienda descargar WAMP SERVER desde su página oficial: http://www.wampserver.com/en/
+-IMPORTANTE: Instalar como administrador.
+
+-Una vez instalado WAMPSERVER, ubicar la ruta de instalaci�n, por defecto C://wamp64, y acceder a la carpeta www dentro
+de esta (C://wamp64/www)
+
+-Aquí se debe realizar la clonación del repositorio, con lo que la carpeta del proyecto quedaría como: C://wamp64/www/GPI2
+
+##Para ejecutar:
+-Iniciar WAMPSERVER (esperar a que el ícono esté de color verde).
+-Luego, en el navegador acceder a http://localhost/GIP2/index.php
+
+##Importar base de datos
+-Iniciar WAMPSERVER
+-Acceder a http://localhost/phpmyadmin/index.php
+-Iniciar sesión con user: root, sin password por defecto.
---
+++ main.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-clearmin.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/roboto.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/material-design.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/small-n-flat.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+        <title>GPI - Login</title>
+    </head>
+    <body class="cm-no-transition cm-1-navbar">
+        <div id="cm-menu">
+            <nav class="cm-navbar cm-navbar-primary">
+                <div class="cm-flex"><a href="main.php" class="cm-logo"></a></div>
+                <div class="btn btn-primary md-menu-white" data-toggle="cm-menu"></div>
+            </nav>
+            <div id="cm-menu-content">
+                <div id="cm-menu-items-wrapper">
+                    <div id="cm-menu-scroller">
+                        <ul class="cm-menu-items">
+                            <li class="active"><a href="main.php" class="sf-house">Pagina Principal</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <header id="cm-header">
+            <nav class="cm-navbar cm-navbar-primary">
+                <div class="btn btn-primary md-menu-white hidden-md hidden-lg" data-toggle="cm-menu"></div>
+                <div class="cm-flex">
+                    <h1>Página Principal</h1>
+                </div>
+                <div class="dropdown pull-right">
+                    <button class="btn btn-primary md-notifications-white" data-toggle="dropdown"> <span class="label label-danger">NUM</span> </button>
+                    <div class="popover cm-popover bottom">
+                        <div class="arrow"></div>
+                        <div class="popover-content">
+                            <div class="list-group">
+                                <a href="#" class="list-group-item">
+                                    <h4 class="list-group-item-heading text-overflow">
+                                        <i class="fa fa-fw fa-envelope"></i> Ejemplo Notificacion 1
+                                    </h4>
+                                    <p class="list-group-item-text text-overflow">Breve Descripción</p>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    <h4 class="list-group-item-heading">
+                                        <i class="fa fa-fw fa-envelope"></i> Ejemplo Notificacion 2
+                                    </h4>
+                                    <p class="list-group-item-text">Breve Descripción</p>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    <h4 class="list-group-item-heading">
+                                        <i class="fa fa-fw fa-warning"></i> Ejemplo Notificacion 3
+                                    </h4>
+                                    <p class="list-group-item-text">Breve Descripción</p>
+                                </a>
+                            </div>
+                            <div style="padding:10px"><a class="btn btn-success btn-block" href="#">Mostrar más</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="dropdown pull-right">
+                    <button class="btn btn-primary md-account-circle-white" data-toggle="dropdown"></button>
+                    <ul class="dropdown-menu">
+                        <li class="disabled text-center">
+                            <a style="cursor:default;"><strong>NOMBRE_USUARIO</strong></a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-user"></i> Perfil</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-cog"></i> Ajustes</a>
+                        </li>
+                        <li>
+                            <a href="index.php"><i class="fa fa-fw fa-sign-out"></i> Cerrar Sesión</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div id="global">
+            <div class="container-fluid cm-container-white">
+                <h2 style="margin-top:0;">Bienvenido a GPI Inventarios.</h2>
+                <p>Descripción.</p>
+                <p>Funcionalidades a la vista.</p>
+            </div>
+            <footer class="cm-footer"><span class="pull-left">Conectado como: NOMBRE_USUARIO</span><span class="pull-right">&copy; PAOMEDIA SARL</span><span class="pull-right">&copy; JIP -</span></footer>
+        </div>
+        <script src="assets/js/lib/jquery-2.1.3.min.js"></script>
+        <script src="assets/js/jquery.mousewheel.min.js"></script>
+        <script src="assets/js/jquery.cookie.min.js"></script>
+        <script src="assets/js/fastclick.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/clearmin.min.js"></script>
+        <script src="assets/js/demo/home.js"></script>
+    </body>
+</html>
